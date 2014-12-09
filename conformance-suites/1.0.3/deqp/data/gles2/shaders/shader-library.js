fe7107e550ca8bef090d2f7a37da172b12204288
Robert <github.fv4s@social.robjh.com>
2014-12-09 15:24:31 +0000

    added float, int and string parse functions.

--- conformance-suites/1.0.3/deqp/data/gles2/shaders/shader-library.js
+++ conformance-suites/1.0.3/deqp/data/gles2/shaders/shader-library.js
@@ -22,21 +22,121 @@
 */
 var shaderLibrary = (function() {
 	'use strict';
-
-	function generateTestCases() {
+	
+	
+	var generateTestCases = function() {
 		stateMachine.getState().testCases = stateMachine.getState().testFile.match(/[^\r\n]+/g);
 		return true;
-	}
+	};
 
-	function processTestFile() {
+	var processTestFile = function() {
 		if (generateTestCases()) {
 			stateMachine.runCallback(shaderLibraryCase.runTestCases);
 		} else {
 			terminate(false);
 		}
-	}
+	};
+
+
+	
+	/* Parser class */
+	var Parser = function() {
+		
+		
+	/* public members */
+		this.parse = function() {
+			
+		};
+		
+	/* private members */
+		
+		var m_curPtr;
+		
+		var parseError         = function(errorStr) {
+			// throws an exception
+//			throw "Parser error: " + errorStr + " near " + m_curPtr.substr(0, 80);
+		};
+		var parseFloatLiteral  = function(str) {
+			return parseFloat(str);
+		};
+		var parseIntLiteral    = function(str) {
+			return parseInt(str);
+		};
+		var parseStringLiteral = function(str) {
+		
+			// strip quotes, replace \n and \t with nl and tabs respectively
+			var endchar = str.substr(0,1);
+			var index_end = 0;
+			do {
+				index_end = str.indexOf(endchar, index_end + 1);
+			} while(index_end >= 0 && str.charAt(index_end - 1) == '\\');
+			
+			if (index_end <= 0) {
+				index_end = str.length;
+			}
+			
+			return str
+				.substr(1, index_end - 1)
+				.replace("\\n", '\n')
+				.replace("\\t", '\t');
+			
+		};
+		var parseShaderSource  = function(str) {
+			
+		};
+		var advanceToken       = function() {
+			
+		};
+	//	var advanceToken       = function(tokenAssumed) {
+	//		
+	//	};
+		var assumeToken        = function(token) {
+			
+		};
+		var mapDataTypeToken   = function(token) {
+			
+		};
+		var getTokenName       = function(token) {
+			
+		};
+		
+//		var parseValueElement  = function(DataType dataType, ShaderCase::Value& result);
+//		var parseValue         = function(ShaderCase::ValueBlock& valueBlock);
+//		var parseValueBlock    = function(ShaderCase::ValueBlock& valueBlock);
+//		var parseShaderCase    = function(vector<tcu::TestNode*>& shaderNodeList);
+//		var parseShaderGroup   = function(vector<tcu::TestNode*>& shaderNodeList);
+
+		// uncomment to expose private functions
+		(function(obj) {
+			obj.priv = {
+				m_curPtr:           m_curPtr,
+				
+				parseError:         parseError,
+				parseFloatLiteral:  parseFloatLiteral,
+				parseIntLiteral:    parseIntLiteral,
+				parseStringLiteral: parseStringLiteral,
+				parseShaderSource:  parseShaderSource,
+				advanceToken:       advanceToken,
+				assumeToken:        assumeToken,
+				mapDataTypeToken:   mapDataTypeToken,
+				getTokenName:       getTokenName,
+				
+//				parseValueElement:  parseValueElement,
+//				parseValue:         parseValue,
+//				parseValueBlock:    parseValueBlock,
+//				parseShaderCase:    parseShaderCase,
+//				parseShaderGroup:   parseShaderGroup,
+				
+				none: false
+			};
+		}(this));
+		//*/
+	};
+
+
 
 	return {
+		Parser:          Parser,
 		processTestFile: processTestFile
 	};
 
