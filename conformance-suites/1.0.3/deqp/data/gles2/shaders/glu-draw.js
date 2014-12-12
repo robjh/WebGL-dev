3062be0a3e997f562c1c3eaec2e5e6301d676f36
Janusz Sobczak <janusz.sobczak@mobica.com>
2014-12-12 13:37:24 -0600

    Changed copyright headers.
    And smuggled one change from 0 to null.
    
    Signed-off-by: Janusz Sobczak <janusz.sobczak@mobica.com>

--- conformance-suites/1.0.3/deqp/data/gles2/shaders/glu-draw.js
+++ conformance-suites/1.0.3/deqp/data/gles2/shaders/glu-draw.js
@@ -1,4 +1,22 @@
-
+/*-------------------------------------------------------------------------
+ * drawElements Quality Program OpenGL ES Utilities
+ * ------------------------------------------------
+ *
+ * Copyright 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 /**
  * Description of a vertex array binding
@@ -43,7 +61,7 @@ var draw = function(gl, program, vertexArrays, primitives, callback) {
 		if (callback)
 			callback.afterDrawCall();
 
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, 0);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 	} else {
 	/* TODO: implement */
 	/*
