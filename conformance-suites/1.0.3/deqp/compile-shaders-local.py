fcc00ccd098465af77392a1b76238d858779f626
albertols <alberto.lopez@mobica.com>
2015-01-05 17:06:12 +0000

    Update compile-shaders-local.py
    paste the script in the desired local directory along with the compiler.jar downloaded from the link in the header comment

--- conformance-suites/1.0.3/deqp/compile-shaders-local.py
+++ conformance-suites/1.0.3/deqp/compile-shaders-local.py
@@ -4,10 +4,33 @@ Created on 23 Dec 2014
 @author:    Alberto Lopez
             Mobica LTD
 '''
-
 '''
 
-DRAFT VERSION TO COMPILE JAVASCRIPTS WITH closure-compiler.jar locally
+Python Script TO COMPILE JAVASCRIPTS WITH closure.jar locally in the selected directory
+
+STEPS:
+- paste this script in the folder where the JavaScripts are contained
+- paste a copy of the compiler.jar found on https://developers.google.com/closure/compiler/docs/gettingstarted_app
+- execute this script
+
+FEATURES:
+1) By Default, this script compiles each JavaScript contained in the folder with the 3 different Closure Compiler levels available
+WHITESPACE_ONLY, SIMPLE_OPTIMIZATIONS and ADVANCED_OPTIMIZATIONS
+
+These compilations levels are represented in the main program as:
+- whitespace= True (True when this compilation level is undertaken)
+- simple= True (True when this compilation level is undertaken)
+- advanced= True (True when this compilation level is undertaken)
+
+2) Each JavaScript compiled generates a .txt file with the corresponding output from the Closure Compiler.
+
+3) By using this script and the Closure Compiler Application(closure.jar), the Warning and Error report Output is always more restrictive than
+the output obtained from the Closure Compiler Service UI (http://closure-compiler.appspot.com/home).
+
+4) If there are no errors in the compiled JavaScript, a copy of the compressed JavaScript is returned in its corresponding field within
+the .txt file generated.
+
+5) By executing this Python script, NONE .js output files are generated, to avoid compilation of generated js in the local directory while running this script
 
 '''
 
@@ -17,81 +40,74 @@ import subprocess
 import threading 
 from sys import stdout, stderr
 
-shaders_directory= "C:\\Users\\allo\\Documents\\GitHub\\WebGL-dev\\conformance-suites\\1.0.3\\deqp\\data\\gles2\\shaders"
-cmdInput= "java -jar compiler.jar --compilation_level SIMPLE --js glu-draw.js --js_output_file gluDrawCompiled.js --warning_level VERBOSE"
+#simpleCompilationCmdInput= "java -jar compiler.jar --compilation_level SIMPLE --js glu-draw.js --js_output_file gluDrawCompiled.js --warning_level VERBOSE"
 
-def getShadersJavaScript(directory):
+def getShadersJavaScript(whitespaceCompilation, simpleCompilation, advancedCompilation):
     
+    directory=os.getcwd()
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".js"):
+                compileJavaScript(file, whitespaceCompilation, simpleCompilation, advancedCompilation)
                 
-                #to check script's functionality only returns the first one found (glu-draw.js)
-                return file
 
+def compileJavaScript(file, whitespaceCompilation, simpleCompilation, advancedCompilation):
+    print("RUNNING CLOSURE COMPILER OUTPUT for " +file)
 
-
-def tee_pipe(pipe, f1):
-            for line in pipe:
-                f1.write(line)
-
-jsFile=getShadersJavaScript(shaders_directory)
-
-print("CLOSURE COMPILER OUTPUT " +jsFile)
-
-
-outputCompilerFile= jsFile.strip()[0:-3]
-outputCompilerFile= outputCompilerFile +".txt"
-
-
-with open(outputCompilerFile, "w") as out_file:
-        out_file.write("CLOSURE COMPILER OUTPUT " + "\n")
-        out_file.write("JavaScript shader file: " + outputCompilerFile + "\n" + "\n")
-        #out_file.write("COMPILATION LEVEL: " + compilationLevel + "\n")
-        #out_file.write("OUTPUT FORMAT: " + outputFormat + "\n")
-        #out_file.write("OUTPUT INFO: " + outputInfo + "\n")
-        
-        out_file.flush()
-
-with open(outputCompilerFile, "ab") as out_file:
-        
-        proc = subprocess.Popen(cmdInput, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        #proc.communicate()
-        
-        
-        while proc.poll() is None:
-            line = proc.stdout.readline()
-            out_file.write(line)
-        
-        
-        out_file.flush()
-        proc.wait()
-        
-        proc = subprocess.Popen(cmdInput, shell=True)
-
-'''
-        # SAVING IN THE FILE USING THREADS
-        proc = subprocess.Popen(cmdInput, shell=True, stdout=subprocess.PIPE)
-        
-        t1 = threading.Thread(target=tee_pipe, args=(proc.stdout, out_file))
-
-        t1.start()
-
-        t1.join()
-        proc.wait()
-'''
-        
-        
-'''        
-        proc = subprocess.Popen(cmdInput, stdout=subprocess.PIPE)
-        sys.stdout.write(line)
-        proc.wait()
-        out_file.write("\n" + "Errors in "+ jsFile +" COPIED:" + "\n")
-        out_file.write(stdout.__str__())
-        
-        
-        print(proc)
-        
-        out_file.flush()
-'''
+    outputCompilerFile= file.strip()[0:-3]
+    outputCompilerFile= outputCompilerFile +".txt"
     
+    with open(outputCompilerFile, "w") as out_file:
+            out_file.write("CLOSURE COMPILER OUTPUT " + "\n")
+            out_file.write("JavaScript shader file: " + file + "\n")
+            out_file.write("Output file from CLOSURE COMPILER: " + outputCompilerFile + "\n")
+            out_file.flush()
+    
+    if whitespaceCompilation==True:
+        cmdInput= "java -jar compiler.jar --compilation_level WHITESPACE_ONLY --js "+file+" --warning_level VERBOSE"
+        with open(outputCompilerFile, "a") as out_file:
+            out_file.write("\n"+ "------------------------------------------" + "\n")
+            out_file.write("COMPILATION LEVEL: WHITESPACE_ONLY " + "\n")
+            out_file.flush()
+        writeOutputAmendFile(outputCompilerFile, cmdInput)
+    
+    if simpleCompilation==True:
+        cmdInput= "java -jar compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js "+file+" --warning_level VERBOSE"
+        with open(outputCompilerFile, "a") as out_file:
+            out_file.write("\n"+"\n"+ "------------------------------------------" + "\n")
+            out_file.write("COMPILATION LEVEL: SIMPLE_OPTIMIZATIONS" + "\n")
+            out_file.flush()
+        writeOutputAmendFile(outputCompilerFile, cmdInput)
+        
+    if advancedCompilation==True:
+        cmdInput= "java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js "+file+" --warning_level VERBOSE"
+        with open(outputCompilerFile, "a") as out_file:
+            out_file.write("\n"+"\n"+ "------------------------------------------" + "\n")
+            out_file.write("COMPILATION LEVEL: ADVANCED_OPTIMIZATIONS" + "\n")
+            out_file.flush()
+        writeOutputAmendFile(outputCompilerFile, cmdInput)
+    
+    print("JavaScript " +file + " SUCCESSFULLY COMPILED!")
+    print("Output saved in " +outputCompilerFile + " in current working directory " + os.getcwd() + "\n")
+
+
+def writeOutputAmendFile(outputCompilerFile, cmdInput):
+    with open(outputCompilerFile, "ab") as out_file:
+            
+            proc = subprocess.Popen(cmdInput, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            
+            while proc.poll() is None:
+                line = proc.stdout.readline()
+                out_file.write(line)
+            
+            out_file.flush()
+            proc.wait()
+            #proc = subprocess.Popen(simpleCompilationCmdInput, shell=True)
+
+
+#main program
+whitespace= True
+simple= True
+advanced= True
+getShadersJavaScript(whitespace, simple, advanced)
+print("------ END EXECUTION Python script: compiler-shaders-local.py ------")
