f7afd356443f4f76b0a6e5f93bf60078434de591
Robert <github.fv4s@social.robjh.com>
2015-01-23 13:05:52 +0000

    removed formatting issues from deqp-draw.js

--- conformance-suites/1.0.3/deqp/resources/deqp-draw.js
+++ conformance-suites/1.0.3/deqp/resources/deqp-draw.js
@@ -31,11 +31,11 @@ var deqpDraw = (function() {
  * @param data Source data
  */
 var VertexArrayBinding = function(type, location, components, elements, data) {
-	this.type = type;
-	this.location = location;
-	this.components = components;
-	this.elements = elements;
-	this.data = data;
+    this.type = type;
+    this.location = location;
+    this.components = components;
+    this.elements = elements;
+    this.data = data;
 };
 
 /**
@@ -47,35 +47,35 @@ var VertexArrayBinding = function(type, location, components, elements, data) {
  * @param {function()} callback
  */
 var draw = function(gl, program, vertexArrays, primitives, callback) {
-	/** TODO: finish implementation */
-	/** @type {Array.<WebGLBuffer>} */ var objects = [];
+    /** TODO: finish implementation */
+    /** @type {Array.<WebGLBuffer>} */ var objects = [];
 
-	for (var i = 0; i < vertexArrays.length; i++) {
-		/** @type {WebGLBuffer} */ var buffer = vertexBuffer(gl,vertexArrays[i]);
-		objects.push(buffer);
-	}
+    for (var i = 0; i < vertexArrays.length; i++) {
+        /** @type {WebGLBuffer} */ var buffer = vertexBuffer(gl, vertexArrays[i]);
+        objects.push(buffer);
+    }
 
-	if (primitives.indices) {
-		/** @type {WebGLBuffer} */ var elemBuffer = indexBuffer(gl, primitives);
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elemBuffer);
+    if (primitives.indices) {
+        /** @type {WebGLBuffer} */ var elemBuffer = indexBuffer(gl, primitives);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elemBuffer);
 
-		if (callback)
-			callback.beforeDrawCall();
+        if (callback)
+            callback.beforeDrawCall();
 
-		drawIndexed(gl, primitives, 0);
+        drawIndexed(gl, primitives, 0);
 
-		if (callback)
-			callback.afterDrawCall();
+        if (callback)
+            callback.afterDrawCall();
 
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-	} else {
-	/** TODO: implement */
-	}
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    } else {
+    /** TODO: implement */
+    }
 
-  assertMsgOptions(gl.getError() === gl.NO_ERROR, "drawArrays", false, true);
-	for (var i = 0; i < vertexArrays.length; i++) {
-		gl.disableVertexAttribArray(vertexArrays[i].location);
-	}
+  assertMsgOptions(gl.getError() === gl.NO_ERROR, 'drawArrays', false, true);
+    for (var i = 0; i < vertexArrays.length; i++) {
+        gl.disableVertexAttribArray(vertexArrays[i].location);
+    }
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 };
 
@@ -85,17 +85,16 @@ var draw = function(gl, program, vertexArrays, primitives, callback) {
  * @param {PrimitiveList} primitives Primitives to draw
  * @param {number} offset
  */
-var drawIndexed = function(gl, primitives, offset)
-{
-	var	mode = getPrimitiveGLType(gl, primitives.type);
-	/** TODO: C++ implementation supports different index types, we use only int16.
-		Could it cause any issues?
-		
-		deUint32	indexGLType	= getIndexGLType(primitives.indexType);
-	*/
+var drawIndexed = function(gl, primitives, offset) {
+    var mode = getPrimitiveGLType(gl, primitives.type);
+    /** TODO: C++ implementation supports different index types, we use only int16.
+        Could it cause any issues?
 
-	gl.drawElements(mode, primitives.indices.length, gl.UNSIGNED_SHORT, offset);
-}
+        deUint32 indexGLType = getIndexGLType(primitives.indexType);
+    */
+
+    gl.drawElements(mode, primitives.indices.length, gl.UNSIGNED_SHORT, offset);
+};
 
 
 /**
@@ -103,17 +102,17 @@ var drawIndexed = function(gl, primitives, offset)
  * @enum
  */
 var primitiveType = {
-	TRIANGLES: 0,
-	TRIANGLE_STRIP: 1,
-	TRIANGLE_FAN: 2,
+    TRIANGLES: 0,
+    TRIANGLE_STRIP: 1,
+    TRIANGLE_FAN: 2,
 
-	LINES: 3,
-	LINE_STRIP: 4,
-	LINE_LOOP: 5,
+    LINES: 3,
+    LINE_STRIP: 4,
+    LINE_LOOP: 5,
 
-	POINTS: 6,
+    POINTS: 6,
 
-	PATCHES: 7
+    PATCHES: 7
 };
 
 /**
@@ -123,19 +122,19 @@ var primitiveType = {
  * @return GL primitive type
  */
 var getPrimitiveGLType = function(gl, type) {
-	switch (type) {
-		case primitiveType.TRIANGLES:		return gl.TRIANGLES;
-		case primitiveType.TRIANGLE_STRIP:	return gl.TRIANGLE_STRIP;
-		case primitiveType.TRIANGLE_FAN:	return gl.TRIANGLE_FAN;
-		case primitiveType.LINES:			return gl.LINES;
-		case primitiveType.LINE_STRIP:		return gl.LINE_STRIP;
-		case primitiveType.LINE_LOOP:		return gl.LINE_LOOP;
-		case primitiveType.POINTS:			return gl.POINTS;
-		case primitiveType.PATCHES:			return gl.PATCHES;
-		default:
-			testFailedOptions("Unknown primitive type " + type, true);
-			return undefined;
-	}
+    switch (type) {
+        case primitiveType.TRIANGLES:       return gl.TRIANGLES;
+        case primitiveType.TRIANGLE_STRIP:  return gl.TRIANGLE_STRIP;
+        case primitiveType.TRIANGLE_FAN:    return gl.TRIANGLE_FAN;
+        case primitiveType.LINES:           return gl.LINES;
+        case primitiveType.LINE_STRIP:      return gl.LINE_STRIP;
+        case primitiveType.LINE_LOOP:       return gl.LINE_LOOP;
+        case primitiveType.POINTS:          return gl.POINTS;
+        case primitiveType.PATCHES:         return gl.PATCHES;
+        default:
+            testFailedOptions('Unknown primitive type ' + type, true);
+            return undefined;
+    }
 };
 
 /**
@@ -143,7 +142,7 @@ var getPrimitiveGLType = function(gl, type) {
  * @param {number} indices
  */
 var triangles = function(indices) {
-	return new PrimitiveList(primitiveType.TRIANGLES, indices);
+    return new PrimitiveList(primitiveType.TRIANGLES, indices);
 };
 
 /**
@@ -151,7 +150,7 @@ var triangles = function(indices) {
  * @param {number} indices
  */
 var patches = function(indices) {
-	return new PrimitiveList(primitiveType.PATCHES, indices);
+    return new PrimitiveList(primitiveType.PATCHES, indices);
 };
 
 /**
@@ -160,8 +159,8 @@ var patches = function(indices) {
  * @param {number} indices
  */
 var PrimitiveList = function(type, indices) {
-	this.type = type;
-	this.indices = indices;
+    this.type = type;
+    this.indices = indices;
 };
 
 /**
@@ -171,13 +170,13 @@ var PrimitiveList = function(type, indices) {
  * @return {WebGLBuffer} indexObject buffer with elements
  */
 var indexBuffer = function(gl, primitives) {
-	/** @type {WebGLBuffer} */ var indexObject = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexObject);
-	assertMsgOptions(gl.getError() === gl.NO_ERROR, "bindBuffer", false, true);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(primitives.indices), gl.STATIC_DRAW);
-	assertMsgOptions(gl.getError() === gl.NO_ERROR, "bufferData", false, true);
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-	return indexObject;
+    /** @type {WebGLBuffer} */ var indexObject = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexObject);
+    assertMsgOptions(gl.getError() === gl.NO_ERROR, 'bindBuffer', false, true);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(primitives.indices), gl.STATIC_DRAW);
+    assertMsgOptions(gl.getError() === gl.NO_ERROR, 'bufferData', false, true);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    return indexObject;
 };
 
 /**
@@ -187,67 +186,68 @@ var indexBuffer = function(gl, primitives) {
  * @return {WebGLBuffer} buffer of vertices
  */
 var vertexBuffer = function(gl, vertexArray) {
-	/** @type {WebGLBuffer} */ var buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-	assertMsgOptions(gl.getError() === gl.NO_ERROR, "bindBuffer", false, true);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexArray.data), gl.STATIC_DRAW);
-	assertMsgOptions(gl.getError() === gl.NO_ERROR, "bufferData", false, true);
-	gl.enableVertexAttribArray(vertexArray.location);
-	assertMsgOptions(gl.getError() === gl.NO_ERROR, "enableVertexAttribArray", false, true);
-	gl.vertexAttribPointer(vertexArray.location, vertexArray.components, vertexArray.type, false, 0, 0);
-	assertMsgOptions(gl.getError() === gl.NO_ERROR, "vertexAttribPointer", false, true);
-	_logToConsole(vertexArray);
-	return buffer;
+    /** @type {WebGLBuffer} */ var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    assertMsgOptions(gl.getError() === gl.NO_ERROR, 'bindBuffer', false, true);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexArray.data), gl.STATIC_DRAW);
+    assertMsgOptions(gl.getError() === gl.NO_ERROR, 'bufferData', false, true);
+    gl.enableVertexAttribArray(vertexArray.location);
+    assertMsgOptions(gl.getError() === gl.NO_ERROR, 'enableVertexAttribArray', false, true);
+    gl.vertexAttribPointer(vertexArray.location, vertexArray.components, vertexArray.type, false, 0, 0);
+    assertMsgOptions(gl.getError() === gl.NO_ERROR, 'vertexAttribPointer', false, true);
+    _logToConsole(vertexArray);
+    return buffer;
 };
 
 var Pixel = function(rgba) {
-	this.rgba = rgba;
+    this.rgba = rgba;
 };
 
 Pixel.prototype.getRed = function() {
-	return this.rgba[0];
+    return this.rgba[0];
 };
 Pixel.prototype.getGreen = function() {
-	return this.rgba[1];
+    return this.rgba[1];
 };
 Pixel.prototype.getBlue = function() {
-	return this.rgba[2];
+    return this.rgba[2];
 };
 Pixel.prototype.getAlpha = function() {
-	return this.rgba[4];
+    return this.rgba[4];
 };
 
 var Surface = function() {
 };
 
 Surface.prototype.readSurface = function(gl, x, y, width, height) {
-	this.buffer = new Uint8Array(width * height * 4);
-	gl.readPixels(x, y, width, height, gl.RGBA, gl.UNSIGNED_BYTE, this.buffer);
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
-	return this.buffer;
+    this.buffer = new Uint8Array(width * height * 4);
+    gl.readPixels(x, y, width, height, gl.RGBA, gl.UNSIGNED_BYTE, this.buffer);
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    return this.buffer;
 };
 
 Surface.prototype.getPixel = function(x, y) {
-	/** @type {number} */ var base = (x + y * this.height) * 4;
-	/** @type {Array.<number>} */
-	var rgba = [
-		this.buffer[base],
-		this.buffer[base + 1],
-		this.buffer[base + 2],
-		this.buffer[base + 3]
-		];
-	return new Pixel(rgba);
+    /** @type {number} */ var base = (x + y * this.height) * 4;
+    /** @type {Array.<number>} */
+    var rgba = [
+        this.buffer[base],
+        this.buffer[base + 1],
+        this.buffer[base + 2],
+        this.buffer[base + 3]
+        ];
+    return new Pixel(rgba);
 };
 
 
 return {
-	draw: draw,
-	triangles: triangles,
-	patches: patches,
-	Surface: Surface,
-	VertexArrayBinding: VertexArrayBinding
+    draw: draw,
+    triangles: triangles,
+    patches: patches,
+    Surface: Surface,
+    VertexArrayBinding: VertexArrayBinding
 };
 }());
+
