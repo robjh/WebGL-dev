896aac4f075e2cd754c84044741d8cefd877d77d
marcorp <marco.perez@mobica.com>
2015-01-29 11:27:26 -0600

    First UniformBlockCase.cpp port, added corresponding test. Fixed getPixel bug in deqp-draw.js.
    Signed-off-by: marcorp <marco.perez@mobica.com>

--- sdk/tests/deqp/resources/deqp-draw.js
+++ sdk/tests/deqp/resources/deqp-draw.js
@@ -39,6 +39,83 @@ var VertexArrayBinding = function(type, location, components, elements, data) {
 };
 
 /**
+ * ! Lower named bindings to locations and eliminate bindings that are not used by program.
+ * @param {WebGLRenderingContext} gl WebGL context
+ * @param {number} program ID, ID of the shader program
+ * @param {Array} inputArray, Array with the named binding locations
+ * @param {Array} outputArray, Array with the lowered locations
+ */
+var namedBindingsToProgramLocations = function(gl, program, inputArray, outputArray) {
+    if (typeof outputArray === "undefined")
+        outputArray = [];
+
+    for (var i=0; i<inputArray.length; i++)
+    {
+        var cur = inputArray[i];
+        if(typeof cur.location === "string")
+        {
+            //assert(binding.location >= 0);
+            var location = gl.getAttribLocation(program.getProgram(), cur.location);
+            if (location >= 0)
+            {
+                // Add binding.location as an offset to accomodate matrices.
+                outputArray.push(new VertexArrayBinding(cur.type, location, cur.components, cur.elements, cur.data));
+            }
+        }
+        else
+        {
+            outputArray.push(cur);
+        }
+    }
+
+    return outputArray;
+};
+
+/**
+ * Creates vertex buffer, binds it and draws elements
+ * @param {WebGLRenderingContext} gl WebGL context
+ * @param {number} program ID, vertexProgramID
+ * @param {Array.<number>} vertexArrays
+ * @param {PrimitiveList} primitives to draw
+ * @param {function()} callback
+ */
+var drawFromBuffers = function(gl, program, vertexArrays, primitives, callback) {
+    /** TODO: finish implementation */
+    /** @type {Array.<WebGLBuffer>} */ var objects = [];
+
+    // Lower bindings to locations
+    vertexArrays = namedBindingsToProgramLocations(gl, program, vertexArrays);
+
+    for (var i = 0; i < vertexArrays.length; i++) {
+        /** @type {WebGLBuffer} */ var buffer = vertexBuffer(gl, vertexArrays[i]);
+        objects.push(buffer);
+    }
+
+    if (primitives.indices) {
+        /** @type {WebGLBuffer} */ var elemBuffer = indexBuffer(gl, primitives);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elemBuffer);
+
+        if (callback)
+            callback.beforeDrawCall();
+
+        drawIndexed(gl, primitives, 0);
+
+        if (callback)
+            callback.afterDrawCall();
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    } else {
+    /** TODO: implement */
+    }
+
+  assertMsgOptions(gl.getError() === gl.NO_ERROR, 'drawArrays', false, true);
+    for (var i = 0; i < vertexArrays.length; i++) {
+        gl.disableVertexAttribArray(vertexArrays[i].location);
+    }
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+};
+
+/**
  * Creates vertex buffer, binds it and draws elements
  * @param {WebGLRenderingContext} gl WebGL context
  * @param {number} program ID, vertexProgramID
@@ -215,6 +292,12 @@ Pixel.prototype.getBlue = function() {
 Pixel.prototype.getAlpha = function() {
     return this.rgba[3];
 };
+Pixel.prototype.equals = function(otherPixel) {
+    return this.rgba[0] == otherPixel.rgba[0] &&
+           this.rgba[1] == otherPixel.rgba[1] &&
+           this.rgba[2] == otherPixel.rgba[2] &&
+           this.rgba[3] == otherPixel.rgba[3];
+}
 
 var Surface = function() {
 };
@@ -230,7 +313,7 @@ Surface.prototype.readSurface = function(gl, x, y, width, height) {
 };
 
 Surface.prototype.getPixel = function(x, y) {
-    /** @type {number} */ var base = (x + y * this.height) * 4;
+    /** @type {number} */ var base = (x + y * this.width) * 4;
     /** @type {Array.<number>} */
     var rgba = [
         this.buffer[base],
@@ -243,7 +326,10 @@ Surface.prototype.getPixel = function(x, y) {
 
 
 return {
+    namedBindingsToProgramLocations: namedBindingsToProgramLocations,
+    drawFromBuffers: drawFromBuffers,
     draw: draw,
+    Pixel: Pixel,
     triangles: triangles,
     patches: patches,
     Surface: Surface,
