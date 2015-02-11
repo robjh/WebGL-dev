6565e66095c7959b9cce574b8f04d230d6c4eb58
Janusz Sobczak <janusz.sobczak@mobica.com>
2015-02-10 18:26:42 -0600

    Implemented getPixel().
    Signed-off-by: Janusz Sobczak <janusz.sobczak@mobica.com>

--- sdk/tests/deqp/framework/common/tcuTexture.js
+++ sdk/tests/deqp/framework/common/tcuTexture.js
@@ -27,6 +27,17 @@ define(['../delibs/debase/deInt32'], function(deInt32)  {
 	};
 	var DE_FALSE = false;
 
+	/* TODO: It's not really a class.
+	 * The code should be refactored to use an array directly
+	 */
+	var Vec4 = function(a, b, c, d) {
+		return [a, b, c, d];
+	};
+	var UVec4 = function(a, b, c, d) {
+		return [a, b, c, d];
+	};
+
+
 	var ChannelOrder = {
 		R: 0,
 		A: 1,
@@ -81,6 +92,42 @@ define(['../delibs/debase/deInt32'], function(deInt32)  {
 		this.type = type;
 	};
 
+/**
+ * Get TypedArray type that can be used to access texture.
+ * @param {ChannelType} type
+ * @return {TypedArray}
+ */
+var getTypedArray = function(type) {
+	switch(type) {
+		case ChannelType.SNORM_INT8: return  Int8Array;
+		case ChannelType.SNORM_INT16: return  Int16Array;
+		case ChannelType.SNORM_INT32: return  Int32Array;
+		case ChannelType.UNORM_INT8: return  Int8Array;
+		case ChannelType.UNORM_INT16: return  Int16Array;
+		case ChannelType.UNORM_INT32: return  Int32Array;
+		case ChannelType.UNORM_SHORT_565: return  Int16Array;
+		case ChannelType.UNORM_SHORT_555: return  Int16Array;
+		case ChannelType.UNORM_SHORT_4444: return  Int16Array;
+		case ChannelType.UNORM_SHORT_5551: return  Int16Array;
+		case ChannelType.UNORM_INT_101010: return  Int32Array;
+		case ChannelType.UNORM_INT_1010102_REV: return  Int32Array;
+		case ChannelType.UNSIGNED_INT_1010102_REV: return  Int32Array;
+		case ChannelType.UNSIGNED_INT_11F_11F_10F_REV: return  Int32Array;
+		case ChannelType.UNSIGNED_INT_999_E5_REV: return  Int32Array;
+		case ChannelType.UNSIGNED_INT_24_8: return  Int32Array;
+		case ChannelType.SIGNED_INT8: return  Int8Array;
+		case ChannelType.SIGNED_INT16: return  Int16Array;
+		case ChannelType.SIGNED_INT32: return  Int32Array;
+		case ChannelType.UNSIGNED_INT8: return  Uint8Array;
+		case ChannelType.UNSIGNED_INT16: return  Uint16Array;
+		case ChannelType.UNSIGNED_INT32: return  Uint32Array;
+		case ChannelType.HALF_FLOAT: return  Uint16Array;
+		case ChannelType.FLOAT: return  Float32Array;
+		case ChannelType.FLOAT_UNSIGNED_INT_24_8_REV: return  Float32Array;
+	}
+
+	throw new Error('Unrecognized type ' + type);
+};
 
 TextureFormat.prototype.getPixelSize = function() {
 	if (this.type == null || this.order == null)
@@ -193,6 +240,89 @@ DeqpArrayBuffer.prototype.size = function() {
 		return this.size() == 0;
 	};
 
+/*
+ * @enum
+ * The values are negative to avoid conflict with channels 0 - 3
+ */
+var channel = {
+	ZERO: -1,
+	ONE: -2,
+};
+
+var getChannelReadMap = function(order) {
+	switch (order) {
+	/*static const Channel INV[]	= { channel.ZERO,	channel.ZERO,	channel.ZERO,	channel.ONE }; */
+
+	case ChannelOrder.R: return    [0,		channel.ZERO,	channel.ZERO,	channel.ONE ];
+	case ChannelOrder.A: return  [ channel.ZERO,	channel.ZERO,	channel.ZERO,	0	];
+	case ChannelOrder.I: return  [ 0,		0,		0,		0	];
+	case ChannelOrder.L: return  [ 0,		0,		0,		channel.ONE	];
+	case ChannelOrder.LA: return  [ 0,		0,		0,		1	];
+	case ChannelOrder.RG: return  [ 0,		1,		channel.ZERO,	channel.ONE	];
+	case ChannelOrder.RA: return  [ 0,		channel.ZERO,	channel.ZERO,	1	];
+	case ChannelOrder.RGB: return  [ 0,		1,		2,		channel.ONE	];
+	case ChannelOrder.RGBA: return  [ 0,		1,		2,		3	];
+	case ChannelOrder.BGRA: return  [ 2,		1,		0,		3	];
+	case ChannelOrder.ARGB: return  [ 1,		2,		3,		0	];
+	case ChannelOrder.D: return  [ 0,		channel.ZERO,	channel.ZERO,	channel.ONE	];
+	case ChannelOrder.S: return  [ channel.ZERO,	channel.ZERO,	channel.ZERO,	0	];
+	case ChannelOrder.DS: return  [ 0,		channel.ZERO,	channel.ZERO,	1	];
+	}
+
+	throw new Error('Unrecognized order ' + order);
+};
+
+var getChannelSize = function(/* ChannelType */ type)
+{
+	switch(type) {
+		case ChannelType.SNORM_INT8: return 			 1;
+		case ChannelType.SNORM_INT16: return 		 2;
+		case ChannelType.SNORM_INT32: return 		 4;
+		case ChannelType.UNORM_INT8: return 			 1;
+		case ChannelType.UNORM_INT16: return 		 2;
+		case ChannelType.UNORM_INT32: return 		 4;
+		case ChannelType.SIGNED_INT8: return 		 1;
+		case ChannelType.SIGNED_INT16: return 		 2;
+		case ChannelType.SIGNED_INT32: return 		 4;
+		case ChannelType.UNSIGNED_INT8: return 		 1;
+		case ChannelType.UNSIGNED_INT16: return 		 2;
+		case ChannelType.UNSIGNED_INT32: return 		 4;
+		case ChannelType.HALF_FLOAT: return 			 2;
+		case ChannelType.FLOAT: return 				 4;
+
+	};
+	throw new Error('Unrecognized type ' + type);
+};
+
+var channelToNormFloat = function(src, bits) {
+	var maxVal = (1 << bits) - 1;
+	return src / maxVal;
+};
+
+var channelToFloat = function(value, /*ChannelType*/ type)
+{
+	switch (type)
+	{
+		case ChannelType.SNORM_INT8:			return Math.max(-1, value / 127);
+		case ChannelType.SNORM_INT16:		return Math.max(-1, value / 32767);
+		case ChannelType.SNORM_INT32:		return Math.max(-1, value / 2147483647);
+		case ChannelType.UNORM_INT8:			return value / 255;
+		case ChannelType.UNORM_INT16:		return value / 65535;
+		case ChannelType.UNORM_INT32:		return value / 4294967295;
+		case ChannelType.SIGNED_INT8:		return value;
+		case ChannelType.SIGNED_INT16:		return value;
+		case ChannelType.SIGNED_INT32:		return value;
+		case ChannelType.UNSIGNED_INT8:		return value;
+		case ChannelType.UNSIGNED_INT16:		return value;
+		case ChannelType.UNSIGNED_INT32:		return value;
+		case ChannelType.HALF_FLOAT:			return deFloat16To32(value); /*TODO: Implement */
+		case ChannelType.FLOAT:				return value;
+		default:
+			DE_ASSERT(DE_FALSE);
+			return 0;
+	}
+};
+
 /*--------------------------------------------------------------------*//*!
  * \brief Read-only pixel data access
  *
@@ -208,12 +338,15 @@ var  ConstPixelBufferAccess = function(descriptor) {
 		this.m_format = descriptor.format;
 		this.m_width = descriptor.width;
 		this.m_height = descriptor.height;
-		this.m_depth = descriptor.depth;
+		if (descriptor.depth)
+			this.m_depth = descriptor.depth;
+		else
+			this.m_depth = 1;
 		this.m_data = descriptor.data;
 		if (descriptor.rowPitch)
 			this.m_rowPitch = descriptor.rowPitch;
 		else
-			this.m_rowPitch = this.width * this.m_format.getPixelSize();
+			this.m_rowPitch = this.m_width * this.m_format.getPixelSize();
 
 		if (descriptor.slicePitch)
 			this.m_slicePitch = descriptor.slicePitch;
@@ -225,9 +358,84 @@ var  ConstPixelBufferAccess = function(descriptor) {
 	ConstPixelBufferAccess.prototype.getDataSize = function() { return this.m_depth * this.m_slicePitch;	};
 
 	ConstPixelBufferAccess.prototype.getPixel = function(x, y, z) {
-		/* TODO: implement */
-		throw new Error('Not implemented');
+		if (z == null)
+			z = 0;
+	DE_ASSERT(deInt32.deInBounds32(x, 0, this.m_width));
+	DE_ASSERT(deInt32.deInBounds32(y, 0, this.m_height));
+	DE_ASSERT(deInt32.deInBounds32(z, 0, this.m_depth));
+
+	var				pixelSize		= this.m_format.getPixelSize();
+	var arrayType = getTypedArray(this.m_format.type);
+	var offset = z * this.m_slicePitch + y * this.m_rowPitch + x * pixelSize;
+	var pixelPtr = new arrayType(this.m_data, z * this.m_slicePitch + y * this.m_rowPitch + x * pixelSize);
+
+	var ub = function(pixel, offset, count) {
+		return (pixel >> offset) & ((1 << count) - 1);
 	};
+	var nb = function(pixel, offset, count) {
+		return channelToNormFloat(ub(pixel, offset, count), count);
+	};
+
+	var pixel = pixelPtr[0];
+
+	// Packed formats.
+	switch (this.m_format.type)
+	{
+		case ChannelType.UNORM_SHORT_565:			return Vec4(nb(pixel, 11,  5), nb(pixel,  5,  6), nb(pixel,  0,  5), 1);
+		case ChannelType.UNORM_SHORT_555:			return Vec4(nb(pixel, 10,  5), nb(pixel,  5,  5), nb(pixel,  0,  5), 1);
+		case ChannelType.UNORM_SHORT_4444:			return Vec4(nb(pixel, 12,  4), nb(pixel,  8,  4), nb(pixel,  4,  4), nb(pixel,  0, 4));
+		case ChannelType.UNORM_SHORT_5551:			return Vec4(nb(pixel, 11,  5), nb(pixel,  6,  5), nb(pixel,  1,  5), nb(pixel,  0, 1));
+		case ChannelType.UNORM_INT_101010:			return Vec4(nb(pixel, 22, 10), nb(pixel, 12, 10), nb(pixel,  2, 10), 1);
+		case ChannelType.UNORM_INT_1010102_REV:		return Vec4(nb(pixel,  0, 10), nb(pixel, 10, 10), nb(pixel, 20, 10), nb(pixel, 30, 2));
+		case ChannelType.UNSIGNED_INT_1010102_REV:	return UVec4(ub(pixel, 0, 10), ub(pixel, 10, 10), ub(pixel, 20, 10), ub(pixel, 30, 2));
+		case ChannelType.UNSIGNED_INT_999_E5_REV:	return undefined; /* TODO: Port unpackRGB999E5(*((const deUint32*)pixelPtr));  */
+
+		case ChannelType.UNSIGNED_INT_24_8:
+			switch (this.m_format.order)
+			{
+				// \note Stencil is always ignored.
+				case ChannelType.D:	return Vec4(nb(pixel, 8, 24), 0, 0, 1);
+				case ChannelType.DS:	return Vec4(nb(pixel, 8, 24), 0, 0, 1 /* (float)ub(0, 8) */);
+				default:
+					DE_ASSERT(false);
+			}
+
+		case ChannelType.FLOAT_UNSIGNED_INT_24_8_REV:
+		{
+			DE_ASSERT(this.m_format.order == ChannelType.DS);
+			// \note Stencil is ignored.
+			return Vec4(pixel, 0, 0, 1);
+		}
+
+		case ChannelType.UNSIGNED_INT_11F_11F_10F_REV:
+			return undefined;
+			/* TODO: Implement
+			return Vec4(Float11(ub(pixel, 0, 11)).asFloat(), Float11(ub(pixel, 11, 11)).asFloat(), Float10(ub(pixel, 22, 10)).asFloat(), 1);
+			*/
+
+		default:
+			break;
+	}
+
+	// Generic path.
+	var			result  = [];
+	result.length = 4;
+	var	channelMap	= getChannelReadMap(this.m_format.order);
+	var				channelSize	= getChannelSize(this.m_format.type);
+
+	for (var c = 0; c < 4; c++)
+	{
+		var map = channelMap[c];
+		if (map == channelMap.ZERO)
+			result[c] = 0;
+		else if (map == channelMap.ONE)
+			result[c] = 1;
+		else
+			result[c] = channelToFloat(pixelPtr[map], this.m_format.type);
+	}
+
+	return result;
+};
 
 	/* TODO: do we need any of these? */
 	{
@@ -371,6 +579,7 @@ Texture2D.prototype.constructor = Texture2D;
 		DeqpArrayBuffer: DeqpArrayBuffer,
 		/* TODO: remove - it shouldn't be exported */
 		TextureLevelPyramid: TextureLevelPyramid,
+		ConstPixelBufferAccess: ConstPixelBufferAccess,
 		Texture2D: Texture2D
 	};
 });
