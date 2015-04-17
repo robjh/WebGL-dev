55101d72820d3105e4a15ca2da817b72625ce925
Marcos Lopez <marcos.lopez@mobica.com>
2015-04-17 10:37:15 -0500

    Add extern file with additional definitions
    Signed-off-by: Marcos Lopez <marcos.lopez@mobica.com>

---
+++ sdk/tests/deqp/compiler_additional_extern.js
@@ -0,0 +1,38 @@
+
+
+
+/**
+ * @param {string} msg
+ */
+function description(msg){};
+
+function finishTest(){};
+
+/**
+ * @param {string} name
+ */
+function setCurrentTestName(name){};
+
+/**
+ * @param {string} msg
+ */
+function bufferedLogToConsole(msg){};
+
+/**
+ * @constructor
+ * @param {string} message The error message.
+ */
+var TestFailedException = function (message) {};
+
+/**
+ * @param {boolean} assertion
+ * @param {string} msg
+ * @param {boolean} verbose
+ * @param {boolean} exthrow
+ */
+function assertMsgOptions(assertion, msg, verbose, exthrow) {};
+
+/**
+ * @param {string} msg
+ */
+function debug(msg){};
