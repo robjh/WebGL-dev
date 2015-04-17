520f644733d6d186335db8c2f81394a4d4fc41d8
Marcos Lopez <marcos.lopez@mobica.com>
2015-04-17 11:01:18 -0500

    Updates to extern file
    Signed-off-by: Marcos Lopez <marcos.lopez@mobica.com>

--- sdk/tests/deqp/compiler_additional_extern.js
+++ sdk/tests/deqp/compiler_additional_extern.js
@@ -1,5 +1,123 @@
 
+/** @type {number} */
+WebGLRenderingContextBase.BROWSER_DEFAULT_WEBGL;
 
+/** @type {number} */ WebGLRenderingContextBase.RGBA16;
+                                                                                                    
+
+/** @type {number} */ WebGLRenderingContextBase.RGBA16_SNORM;
+                                                                                                    
+
+/** @type {number} */ WebGLRenderingContextBase.RGB16;
+                                                                                                   
+
+/** @type {number} */ WebGLRenderingContextBase.RGB16_SNORM;
+                                                                                                   
+
+/** @type {number} */ WebGLRenderingContextBase.RGB10;
+                                                                                                             
+
+/** @type {number} */ WebGLRenderingContextBase.RG16;
+                                                                                                  
+
+/** @type {number} */ WebGLRenderingContextBase.RG16_SNORM;
+                                                                                                  
+
+/** @type {number} */ WebGLRenderingContextBase.R16;
+                                                                                                 
+
+/** @type {number} */ WebGLRenderingContextBase.R16_SNORM;
+                                                                                                 
+
+/** @type {number} */ WebGLRenderingContextBase.DEPTH_COMPONENT32;
+                                                                                                    
+
+/** @type {number} */ WebGLRenderingContextBase.ETC1_RGB8_OES;
+                                                              
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_4x4_KHR;
+                                                                  
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_5x4_KHR;
+                                                                  
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_5x5_KHR;
+                                                                  
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_6x5_KHR;
+                                                                  
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_6x6_KHR;
+                                                                  
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_8x5_KHR;
+                                                                  
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_8x6_KHR;
+                                                                  
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_8x8_KHR;
+                                                                  
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_10x5_KHR;
+                                                                   
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_10x6_KHR;
+                                                                   
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_10x8_KHR;
+                                                                   
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_10x10_KHR;
+                                                                    
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_12x10_KHR;
+                                                                    
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_RGBA_ASTC_12x12_KHR;
+                                                                    
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_4x4_KHR;
+                                                                          
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_5x4_KHR;
+                                                                          
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_5x5_KHR;
+                                                                          
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_6x5_KHR;
+                                                                          
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_6x6_KHR;
+                                                                          
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_8x5_KHR;
+                                                                          
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_8x6_KHR;
+                                                                          
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_8x8_KHR;
+                                                                          
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_10x5_KHR;
+                                                                           
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_10x6_KHR;
+                                                                           
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_10x8_KHR;
+                                                                           
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_10x10_KHR;
+                                                                            
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_12x10_KHR;
+                                                                            
+
+/** @type {number} */ WebGLRenderingContextBase.COMPRESSED_SRGB8_ALPHA8_ASTC_12x12_KHR;
+                                                                            
 
 /**
  * @param {string} msg
