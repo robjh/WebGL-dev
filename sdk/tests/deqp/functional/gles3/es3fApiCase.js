5736cb3f5b339b90e03544ec0218eb1e20473e8e
Robert Heywood <github.fv4s@social.robjh.com>
2015-05-27 19:01:12 +0100

    adding implentation for ApiCase

---
+++ sdk/tests/deqp/functional/gles3/es3fApiCase.js
@@ -0,0 +1,106 @@
+'use strict';
+goog.provide('functional.gles3.es3fApiCase');
+goog.require('framework.opengl.gluStrUtil');
+goog.require('framework.common.tcuTestCase');
+
+goog.scope(function() {
+
+    var es3fApiCase = functional.gles3.es3fApiCase;
+    var gluStrUtil  = framework.opengl.gluStrUtil;
+    var tcuTestCase = framework.common.tcuTestCase;
+
+    /**
+    * Base class for all the API tests.
+    * @constructor
+    * @extends {tcuTestCase.DeqpTest}
+    * @param {string} name
+    * @param {string} desc
+    */
+    es3fApiCase.ApiCase = function(name, desc, gl) {
+        gl = gl || window.gl;
+        if (this.test === undefined) {
+            throw new Error('Unimplemented virtual function: es3fApiCase.ApiCase.test')
+        }
+        tcuTestCase.DeqpTest.call(this, name, desc);
+        
+        this.m_gl = gl;
+        this.m_pass = true;
+        this.m_comment = this.name + ' ' + this.description;
+        
+    };
+    
+    es3fApiCase.ApiCase.prototype = Object.create(tcuTestCase.DeqpTest.prototype);
+    es3fApiCase.ApiCase.prototype.constructor = es3fApiCase.ApiCase;
+    
+    es3fApiCase.ApiCase.prototype.iterate = function() {
+        
+        this.test();
+        
+        if (this.m_pass)
+            testPassed(this.m_comment);
+        else
+            testFailedOptions(this.m_comment, true);
+            
+        return tcuTestCase.IterateResult.STOP;
+    };
+    
+    /**
+    * @param {Array<number>|number} expected
+    */
+    es3fApiCase.ApiCase.prototype.expectError = function(expected) {
+        if (expected.constructor === Number) 
+            expected = [expected];
+            
+        var err = this.m_gl.getError();
+        
+        if (expected.indexOf(err) === -1) {
+            
+            var msg = 'expected ';
+            
+            if (expected.length > 1)
+                msg += 'one of ';
+            
+            for (var i = 0, l = expected.length ; i < l ; ++i) {
+            //    msg += gluStrUtil.getErrorStr(expected[i]);
+                msg += expected[i];
+                    
+                if (i < l - 2)
+                    msg += ', ';
+                else if (i == l - 2)
+                    msg += ' or ';
+                else
+                    msg += '.';
+            }
+                
+            bufferedLogToConsole(msg);
+                
+            if (this.m_pass) {
+                this.m_comment = msg;
+                this.m_pass = false;
+            }
+        
+        }
+        
+    };
+    
+    /**
+    * Base class for all the API tests.
+    * @constructor
+    * @extends {es3fApiCase.ApiCase}
+    * @param {string} name
+    * @param {string} desc
+    * @param {function(this:es3fApiCase.ApiCaseCallback)} callback
+    */
+    es3fApiCase.ApiCaseCallback = function(name, desc, gl, callback) {
+        this.test = callback;
+        es3fApiCase.ApiCase.call(this, name, desc, gl);
+    };
+    es3fApiCase.ApiCaseCallback.prototype = Object.create(es3fApiCase.ApiCase.prototype);
+    es3fApiCase.ApiCaseCallback.prototype.constructor = es3fApiCase.ApiCaseCallback;
+    
+/*
+    es3fApiCase.ApiCase.prototype.expectError // (error) or (error0, error1)
+    es3fApiCase.ApiCase.prototype.getSupportedExtensions // (number numSupportedValues, number extension, [number] values )
+    es3fApiCase.ApiCase.prototype.checkBooleans // (char value, char expected);
+//*/
+});
--- sdk/tests/deqp/functional/gles3/es3fNegativeBufferApiTests.js
+++ sdk/tests/deqp/functional/gles3/es3fNegativeBufferApiTests.js
@@ -1,25 +1,52 @@
 'use strict';
-goog.provide('functional.gles3.es3fNegativeBuggerApiTests');
+goog.provide('functional.gles3.es3fNegativeBufferApiTests');
 goog.require('functional.gles3.es3fApiCase');
+goog.require('framework.opengl.gluStrUtil');
 goog.require('framework.common.tcuTestCase');
 
 goog.scope(function() {
     
-    var es3fNegativeBuggerApiTests = functional.gles3.es3fNegativeBuggerApiTests;
+    var es3fNegativeBufferApiTests = functional.gles3.es3fNegativeBufferApiTests;
     var es3fApiCase = functional.gles3.es3fApiCase;
+    var gluStrUtil = framework.opengl.gluStrUtil;
     var tcuTestCase = framework.common.tcuTestCase;
     
     /**
     * @param {WebGLRenderingContextBase} gl
     */
-    es3fNegativeBuggerApiTests.init = function(gl) {
+    es3fNegativeBufferApiTests.init = function(gl) {
+    
+        // not implemented, on account of these functions not generating errors in webgl;
+        // gl.deleteBuffers(), gl.createBuffer()
+    
+        var testGroup = tcuTestCase.runner.testCases;
+    
+        testGroup.addChild(new es3fApiCase.ApiCaseCallback(
+            'bind_buffer', 'Invalid gl.bindBuffer() usage', gl,
+            function() {
+                bufferedLogToConsole('gl.INVALID_ENUM is generated if target is not one of the allowable values.');
+                gl.bindBuffer(-1, null);
+                this.expectError(gl.INVALID_ENUM);
+            }
+        ));
         
+        testGroup.addChild(new es3fApiCase.ApiCaseCallback(
+            'buffer_data', 'Invalid glBufferData() usage', gl,
+            function() {
+                var buffer = gl.createBuffer();
+                
+                bufferedLogToConsole('gl.INVALID_ENUM is generated if target is not one of the allowable values.');
+              //  gl.bindBuffer(-1, null);
+                
+                gl.deleteBuffer(buffer);
+            }
+        ));
     }
     
     /**
     * @param {WebGLRenderingContextBase} gl
     */
-    es3fNegativeBuggerApiTests.run() = function(gl) {
+    es3fNegativeBufferApiTests.run = function(gl) {
         var testName = 'multisample';
         var testDescription = 'Multisample tests';
         var state = tcuTestCase.runner;
@@ -31,7 +58,7 @@ goog.scope(function() {
         setCurrentTestName(testName);
         description(testDescription);
         try {
-            es3fNegativeBuggerApiTests.init(gl);
+            es3fNegativeBufferApiTests.init(gl);
             tcuTestCase.runner.runCallback(tcuTestCase.runTestCases);
         } catch (err) {
             console.log(err);
