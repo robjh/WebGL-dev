0b180e5b61e79617eb30bfe76ea8e6d774218ba5
Daniel Andrade <daniel.andrade@mobica.com>
2015-09-09 16:06:33 -0500

    Style fixes

--- sdk/tests/deqp/functional/gles3/es3fStringQueryTests.js
+++ sdk/tests/deqp/functional/gles3/es3fStringQueryTests.js
@@ -4,14 +4,14 @@
  *
  * Copyright 2014 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the 'License');
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -24,7 +24,7 @@ goog.require('framework.common.tcuTestCase');
 goog.require('functional.gles3.es3fApiCase');
 
 goog.scope(function() {
-	var es3fStringQueryTests = functional.gles3.es3fStringQueryTests;
+    var es3fStringQueryTests = functional.gles3.es3fStringQueryTests;
     var tcuTestCase = framework.common.tcuTestCase;
     var es3fApiCase = functional.gles3.es3fApiCase;
 
@@ -40,63 +40,63 @@ goog.scope(function() {
     es3fStringQueryTests.StringQueryTests.prototype.constructor = es3fStringQueryTests.StringQueryTests;
 
     es3fStringQueryTests.StringQueryTests.prototype.init = function() {
-		this.addChild(new es3fApiCase.ApiCaseCallback('renderer', 'RENDERER', gl, function() {
-			/** @type {string} */ var string = /** @type {string} */ (gl.getParameter(gl.RENDERER));
-			this.check(string !== null,
-				'Got invalid string: ' + string);
-		}));
+        this.addChild(new es3fApiCase.ApiCaseCallback('renderer', 'RENDERER', gl, function() {
+            /** @type {string} */ var string = /** @type {string} */ (gl.getParameter(gl.RENDERER));
+            this.check(string !== null,
+                'Got invalid string: ' + string);
+        }));
 
-		this.addChild(new es3fApiCase.ApiCaseCallback('vendor', "VENDOR", gl, function() {
-			/** @type {string} */ var string = /** @type {string} */ (gl.getParameter(gl.VENDOR));
-			this.check(string !== null,
-				'Got invalid string: ' + string);
-		}));
+        this.addChild(new es3fApiCase.ApiCaseCallback('vendor', 'VENDOR', gl, function() {
+            /** @type {string} */ var string = /** @type {string} */ (gl.getParameter(gl.VENDOR));
+            this.check(string !== null,
+                'Got invalid string: ' + string);
+        }));
 
-		this.addChild(new es3fApiCase.ApiCaseCallback('version', "VERSION", gl, function() {
-			/** @type {string} */ var string = /** @type {string} */ (gl.getParameter(gl.VERSION));
-			/** @type {string} */ var referenceString = "WebGL 2.0";
+        this.addChild(new es3fApiCase.ApiCaseCallback('version', 'VERSION', gl, function() {
+            /** @type {string} */ var string = /** @type {string} */ (gl.getParameter(gl.VERSION));
+            /** @type {string} */ var referenceString = 'WebGL 2.0';
 
-			this.check(string !== null && string.indexOf(referenceString) === 0,
-				"Got invalid string prefix: " + string + " expected: " + referenceString);
-		}));
+            this.check(string !== null && string.indexOf(referenceString) === 0,
+                'Got invalid string prefix: ' + string + ' expected: ' + referenceString);
+        }));
 
-		this.addChild(new es3fApiCase.ApiCaseCallback('shading_language_version', "SHADING_LANGUAGE_VERSION", gl, function() {
-			/** @type {string} */ var string = /** @type {string} */ (gl.getParameter(gl.SHADING_LANGUAGE_VERSION));
-			/** @type {string} */ var referenceString = "WebGL GLSL ES 3.00";
+        this.addChild(new es3fApiCase.ApiCaseCallback('shading_language_version', 'SHADING_LANGUAGE_VERSION', gl, function() {
+            /** @type {string} */ var string = /** @type {string} */ (gl.getParameter(gl.SHADING_LANGUAGE_VERSION));
+            /** @type {string} */ var referenceString = 'WebGL GLSL ES 3.00';
 
-			this.check(string !== null, "Got invalid string");
-			this.check(string.indexOf(referenceString) === 0, "Got invalid string prefix");
+            this.check(string !== null, 'Got invalid string');
+            this.check(string.indexOf(referenceString) === 0, 'Got invalid string prefix');
 
-			/** @type {string} */ var tmpString;
-			/** @type {number} */ var glslMajor = 0;
-			/** @type {number} */ var glslMinorDigit1 = 0;
-			/** @type {number} */ var glslMinorDigit2 = 0;
-			/** @type {boolean} */ var digitsAreValid;
-			/** @type {RegExp} */ var versionRegExp = /(WebGL GLSL ES )(\d)\.(\d)(\d)(.*)/;
-			/** @type {Array<string>} */ var stringGroups = string.split(versionRegExp);
-			tmpString = stringGroups[1]; // WebGL GLSL ES
-			glslMajor = parseInt(stringGroups[2], 10); // x
-			glslMinorDigit1 = parseInt(stringGroups[3], 10); // x
-			glslMinorDigit2 = parseInt(stringGroups[4], 10); // x
+            /** @type {string} */ var tmpString;
+            /** @type {number} */ var glslMajor = 0;
+            /** @type {number} */ var glslMinorDigit1 = 0;
+            /** @type {number} */ var glslMinorDigit2 = 0;
+            /** @type {boolean} */ var digitsAreValid;
+            /** @type {RegExp} */ var versionRegExp = /(WebGL GLSL ES )(\d)\.(\d)(\d)(.*)/;
+            /** @type {Array<string>} */ var stringGroups = string.split(versionRegExp);
+            tmpString = stringGroups[1]; // WebGL GLSL ES
+            glslMajor = parseInt(stringGroups[2], 10); // x
+            glslMinorDigit1 = parseInt(stringGroups[3], 10); // x
+            glslMinorDigit2 = parseInt(stringGroups[4], 10); // x
 
-			digitsAreValid = glslMinorDigit1 >= 0 && glslMinorDigit1 <= 9 && glslMinorDigit2 >= 0 && glslMinorDigit2 <= 9;
+            digitsAreValid = glslMinorDigit1 >= 0 && glslMinorDigit1 <= 9 && glslMinorDigit2 >= 0 && glslMinorDigit2 <= 9;
 
-			this.check(digitsAreValid, "Got invalid string format: " + stringGroups);
-		}));
+            this.check(digitsAreValid, 'Got invalid string format: ' + stringGroups);
+        }));
 
-		this.addChild(new es3fApiCase.ApiCaseCallback('extensions', "EXTENSIONS", gl, function() {
-			/** @type {Array<string>} */ var extensions = gl.getSupportedExtensions();
-			this.check(extensions !== null, "Got invalid string");
+        this.addChild(new es3fApiCase.ApiCaseCallback('extensions', 'EXTENSIONS', gl, function() {
+            /** @type {Array<string>} */ var extensions = gl.getSupportedExtensions();
+            this.check(extensions !== null, 'Got invalid string');
 
-			// [dag] check that all extensions from gl.getSupportedExtensions() are found using gl.getExtension()
-			for (var i in extensions) {
-				/** @type {Object} */ var extension = gl.getExtension(extensions[i]);
-				this.check(extension !== null, "Extension query methods are not consistent.")
-			}
+            // [dag] check that all extensions from gl.getSupportedExtensions() are found using gl.getExtension()
+            for (var i in extensions) {
+                /** @type {Object} */ var extension = gl.getExtension(extensions[i]);
+                this.check(extension !== null, 'Extension query methods are not consistent.')
+            }
 
-			// [dag] check that gl.getExtension() returns null for items not in gl.getSupportedExtensions()
-			this.check(gl.getExtension('Random_String') === null, "Extension query methods are not consistent.");
-		}));
+            // [dag] check that gl.getExtension() returns null for items not in gl.getSupportedExtensions()
+            this.check(gl.getExtension('Random_String') === null, 'Extension query methods are not consistent.');
+        }));
 
     };
 
@@ -105,23 +105,23 @@ goog.scope(function() {
     * @param {WebGL2RenderingContext} context
     */
     es3fStringQueryTests.run = function(context) {
-    	gl = context;
-    	//Set up Test Root parameters
-    	var state = tcuTestCase.runner;
-    	state.setRoot(new es3fStringQueryTests.StringQueryTests());
+        gl = context;
+        //Set up Test Root parameters
+        var state = tcuTestCase.runner;
+        state.setRoot(new es3fStringQueryTests.StringQueryTests());
 
-    	//Set up name and description of this test series.
-    	setCurrentTestName(state.testCases.fullName());
-    	description(state.testCases.getDescription());
+        //Set up name and description of this test series.
+        setCurrentTestName(state.testCases.fullName());
+        description(state.testCases.getDescription());
 
-    	try {
-    		//Run test cases
-    		tcuTestCase.runTestCases();
-    	}
-    	catch (err) {
-    		testFailedOptions('Failed to es3fStringQueryTests.run tests', false);
-    		tcuTestCase.runner.terminate();
-    	}
+        try {
+            //Run test cases
+            tcuTestCase.runTestCases();
+        }
+        catch (err) {
+            testFailedOptions('Failed to es3fStringQueryTests.run tests', false);
+            tcuTestCase.runner.terminate();
+        }
     };
 
 });
